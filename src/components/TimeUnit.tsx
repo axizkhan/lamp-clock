071ae5487af171a925d3d214eced9bfdf6269e25
axizkhan <axizkhan786110@gmail.com>
2025-12-23 11:17:28 +0530

    add responsiveness to clock

--- src/components/TimeUnit.tsx
+++ src/components/TimeUnit.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function TimeUnit({ size, current }: { size: number; current: number }) {
   const DecimalUnit = [];
+  const[isMobile,setIsMobile]=useState<boolean>(false);
+  const [offset,setOffeset]=useState<number>(0);
+
   for (let i = 0; i <= size; i++) {
     DecimalUnit.push(i);
   }
-  const ITEM_HEIGHT = 56; // px
+  // const ITEM_HEIGHT = 56; // px
 
-  const offset = 300 - current * ITEM_HEIGHT;
+  useEffect(()=>{
+    function isMobile(){
+    
+      setIsMobile(window.innerWidth<=640);
+      
+      
+    }
+    window.addEventListener("resize",isMobile);
+
+  return()=>{
+    window.removeEventListener("resize",isMobile);
+  }
+  },[])
+
+  useEffect(()=>{
+    console.log("effect second");
+    if(isMobile){
+      setOffeset((300-current*40));
+    }else{
+      setOffeset((300-current*56));
+      
+    }
+  },[isMobile,current])
+
+  // const offset = 300 - current * ITEM_HEIGHT;
+  // console.log(offset);
 
   return (
     <div
@@ -17,16 +45,16 @@ function TimeUnit({ size, current }: { size: number; current: number }) {
       {DecimalUnit.map((unit) =>
         unit === current ? (
           <div
-            className="relative flex items-center justify-center sm:h-14 h-5   rounded-full sm:w-14 w-5 transition-all duration-500 ease-out will-change-transform"
+            className="relative flex items-center justify-center sm:h-14 h-10   rounded-full sm:w-14 w-10 transition-all duration-500 ease-out will-change-transform"
             key={uuid()}>
-            <div className="absolute top-[20%] rounded-full  sm:h-14 sm:w-14 h-5 w-5 bg-gray-900 flex items-center justify-center font-extrabold drop-shadow-xl  transition-all duration-500 ease-out will-change-transform text-amber-400">
+            <div className="absolute sm:top-[20%] top-[16%] rounded-full  sm:h-14 sm:w-14 h-10 w-10 bg-gray-900 flex items-center justify-center font-extrabold drop-shadow-xl  transition-all duration-500 ease-out will-change-transform text-amber-400">
               {unit}
             </div>
-            <div className="absolute top-0  flex items-center sm:h-20 sm:w-20 h-7 w-7 justify-center bg-amber-400/30 backdrop-blur-none  rounded-full  backface-visible transition-transform duration-500 ease-out will-change-transform shadow-xl border border-amber-300/20 drop-shadow-xl/50 shadow-amber-500/50"></div>
+            <div className="absolute top-0  flex items-center sm:h-20 sm:w-20 h-13 w-13 justify-center bg-amber-400/30 backdrop-blur-none  rounded-full  backface-visible transition-transform duration-500 ease-out will-change-transform shadow-xl border border-amber-300/20 drop-shadow-xl/50 shadow-amber-500/50"></div>
           </div>
         ) : (
           <div
-            className="sm:h-14 sm:w-14 h-5 w-5 flex items-center justify-center transition-all duration-500 ease-out will-change-transform text-gray-700"
+            className="sm:h-14 sm:w-14 h-10 w-10 flex items-center justify-center transition-all duration-500 ease-out will-change-transform text-gray-700"
             key={uuid()}>
             {unit}
           </div>
